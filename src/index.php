60e544dd27a1142bd76046e3943799b542d3fca4
JohnChen <paiching01@gmail.com>
2020-06-16 21:03:53 +0800

    first commit

---
+++ src/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>今天要吃啥？</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center">今天要吃啥？</h1>
+    <div class="row">
+        <div class="col-sm-12">
+
+        </div>
+    </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.min.js"></script>
+<script>
+    $(function () {
+
+    })
+</script>
+</body>
+</html>
