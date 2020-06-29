05c9ccbeeee0016f7ae52f44eaa489aa5ddda151
JohnChen <paiching01@gmail.com>
2020-06-30 07:01:47 +0800

    update

---
+++ vue-router/0630/vue-slim-tabs.vue
@@ -0,0 +1,19 @@
+<template>
+    <tabs>
+        <tab title="Vue">
+            This is Vue
+        </tab>
+        <tab title="React">
+            This is React
+        </tab>
+        <tab title="Svelte">
+            This is Svelte
+        </tab>
+    </tabs>
+</template>
+
+<script>
+    module.exports = {}
+</script>
+
+<style src="default.css"></style>
--- vue-router/index2.js
+++ vue-router/index2.js
@@ -1,11 +1,16 @@
 // 1. 定義路由組件，可以從其他文件 import 進來
+import Add from './router/add.vue';
+
 const Foo = {template: '<div>foo</div>'}
 const Bar = {template: '<div>bar</div>'}
+const Add = {template: Add}
 
 // 2. 定義路由
 const routes = [
     {path: '/foo', component: Foo},
-    {path: '/bar', component: Bar}
+    {path: '/bar', component: Bar},
+    {path: '/add', component: Add},
+
 ]
 
 // 3. 創建 router 實例
