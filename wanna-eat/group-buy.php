8629b79c989f6c1a769f7a70162164056aa23b7a
JohnChen <paiching01@gmail.com>
2020-07-02 07:06:51 +0800

    edit group-buy.php

--- wanna-eat/group-buy.php
+++ wanna-eat/group-buy.php
@@ -15,11 +15,13 @@ require_once './php-component/check-login-inner.php';
 // 如果有帶參數 GET
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (!isset($_GET['id'])) exit('<h1>必須指定參數</h1>');
+    show_host($smarty);
     show_table($smarty);
 }
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     group_buy($smarty);
 }
+
 
 // 顯示數據
 function show_table($smarty)
@@ -31,6 +33,41 @@ function show_table($smarty)
 }
 
 
+// 顯示負責人
+function show_host($smarty)
+{
+    $sql = "SELECT * FROM hosts";
+    $result = connect_mysql($sql);
+    while ($rows = mysqli_fetch_assoc($result)) {
+        $hosts[] = $rows;
+    }
+    $smarty->assign('hosts', $hosts);
+}
+
+
+// 團購
+function group_buy($smarty)
+{
+    if (!$_POST['host'] || !$_POST['end_time'] || !$_POST['remark'] || !$_POST['store_name'] || !$_POST['store_phone']) {
+        $GLOBALS['error_message'] = '請正常使用表單';
+        return;
+    }
+    if (empty($_POST['end_time'])) {
+        $GLOBALS['error_message'] = '必須填寫截止時間';
+        return;
+    }
+
+    $group_buy['store_host'] = $_POST['store_host'];
+    $group_buy['store_name'] = $_POST['store_name'];
+    $group_buy['host'] = $_POST['host'];
+    $group_buy['end_time'] = $_POST['end_time'];
+    $group_buy['remark'] = $_POST['remark'];
+
+    // save
+    $sql = "";
+    var_dump($group_buy);
+
+}
 
 
 if (isset($error_message)) {
---
+++ wanna-eat/templates_c/098a19332295f8747eeb75ca8a385cc119341496_0.file.group-buy.tpl.php
@@ -0,0 +1,98 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-07-02 00:45:28
+  from 'D:\xampp\htdocs\wanna-eat\wanna-eat\templates\group-buy.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5efd1208075f84_73626935',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '098a19332295f8747eeb75ca8a385cc119341496' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\wanna-eat\\wanna-eat\\templates\\group-buy.tpl',
+      1 => 1593643523,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:head.tpl' => 1,
+    'file:header.tpl' => 1,
+    'file:js.tpl' => 1,
+  ),
+),false)) {
+function content_5efd1208075f84_73626935 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:head.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+$_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+<main>
+    <div class="container">
+        <!-- 開團 -->
+        <div class="row modal-wrap">
+            <form action="group-buy.php" method="post">
+                <div class="col-sm-12 store-item">
+                    <input type="hidden" name="id" value="orders.id">
+                    <?php if (isset($_smarty_tpl->tpl_vars['error']->value)) {?>
+                        <div class="form-group text-danger">
+                            <?php echo $_smarty_tpl->tpl_vars['error']->value;?>
+
+                        </div>
+                    <?php }?>
+                    <ul class="store-text">
+                        <li>負責人：
+                            <select name="host">
+                                <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['hosts']->value, 'item');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['item']->value) {
+?>
+                                    <option value="<?php echo $_smarty_tpl->tpl_vars['item']->value['host_name'];?>
+"><?php echo $_smarty_tpl->tpl_vars['item']->value['host_name'];?>
+</option>
+                                <?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+                            </select>
+                            <button>新增負責人</button>
+                        </li>
+                        <li>店家名稱：<input type="text" value="<?php echo $_smarty_tpl->tpl_vars['item']->value['name'];?>
+" name="store_name" readonly class="form-control"></li>
+                        <li>店家電話：<input type="text" value="<?php echo $_smarty_tpl->tpl_vars['item']->value['phone'];?>
+" name="store_phone" readonly class="form-control"></li>
+                        <li>簡介：排骨飯比雞排飯好吃，肉很大塊</li>
+                        <li>菜單：<a href="<?php echo $_smarty_tpl->tpl_vars['item']->value['images'];?>
+"
+                                  class="btn btn-outline-primary btn-sm btn-pill px-2 btn-menu">菜單</a></li>
+                        <li class="image_show">
+                            <div class="modal">
+                                <div class="modal-body d-flex align-items-center justify-content-center">
+                                    <div class="img"></div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <label>截止時間：
+                                <input type="text" value="1200" name="end_time" required>*
+                            </label>
+                        </li>
+                        <li>
+                            <label>注意事項：
+                                <input type="text" value="老闆很凶會罵人" name="remark">
+                            </label>
+                        </li>
+                    </ul>
+
+                    <div class="form-group">
+                        <button class="btn btn-danger">開始團購囉</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+<?php $_smarty_tpl->_subTemplateRender("file:js.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
