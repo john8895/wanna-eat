b9438ecfbb7e89121d2b56fcf13753aa57548cdd
john8895 <paiching01@gmail.com>
2021-05-18 09:10:49 +0800

    update

---
+++ wanna-eat/assets/includes/dbh-inc.php
@@ -0,0 +1,20 @@
+<?php
+
+class dbh {
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+
+    protected function connect() {
+        $this->servername = 'localhost';
+        $this->username = 'apai01';
+        $this->password = '123456';
+        $this->dbname = 'wannaeat';
+
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+
+        mysqli_set_charset($conn, 'utf8');
+        return $conn;
+    }
+}
